166ace950d174f287bfa0629c2446e66c3ed4a7e
Hugo Geoffroy <github@pstch.net>
2014-04-25 03:58:21 +0300

    Add metadata and version to __init__.py

---
+++ django_render/__init__.py
@@ -0,0 +1,13 @@
+"""
+Django render provides a template tag that works similar to an inclusion tag, 
+however it will attempt to use a custom template for each model. This is useful 
+when iterating over a list of heterogeneous model instances or a queryset with 
+generic relationships.
+
+"""
+__title__ = 'django-render'
+__version__ = '0.1.1'
+__author__ = 'Lincoln Loop'
+__license__ = 'See LICENSE file'
+__copyright__ = """Copyright (c) Lincoln Loop, LLC and individual contributors.
+All rights reserved."""
